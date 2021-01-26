27f9dbd6273738488466a9ecabb55a283b46f1db
Arthur Couto <arthurcouto010@gmail.com>
2021-01-26 18:45:04 +0000

    JKDBW

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
+// const express = require('express')
+// const app = express()
+// const PORT = process.env.PORT || 3000
+
+// const bodyParser = require('body-parser')
+
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: false }))
+
+// app.get('/', (req, res) => {
+//     res.send('OK')
+// })
+
+// app.listen(PORT)
+
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000
-
-const bodyParser = require('body-parser')
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.send('OK')
+    res.send('Hello World!')
 })
 
-app.listen(PORT)
+app.listen(port, () => {
+    console.log("hello world!")
+})
