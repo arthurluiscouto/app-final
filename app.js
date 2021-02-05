f61c21248b31e73a60be05785434258288db0bfd
Arthur Couto <arthurcouto010@gmail.com>
2021-02-05 13:56:19 +0000

    update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 const data = require("./data.json")
 const cors = require("cors")
 
